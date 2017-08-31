d9798a04746e7534fe309c92d58dc407af25356e
uxster <hello@uxster.nl>
2017-08-31 15:00:18 +0200

    checks off every item on click, need to fix that

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(() => {
 	$('#popup').modal();
+
 	$(".button-collapse").sideNav({
 		menuWidth: 200
 	});
+
 	$('.slider').slider({
 		indicators: false
 	});	
+
+	$('.checkoff').click(() => {
+		$('.bujobody').addClass('strike');
+	});
+
+	// $().dblclick(() => {
+	// 	$()
+	// });
 })
